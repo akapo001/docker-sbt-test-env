03ea91f4b87a2d7e4dff29ebee0fb47e58e9ea80
negokaz <negokaz@gmail.com>
2019-07-19 01:08:52 +0900

    fix: wait until started docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,10 @@
 
 /usr/local/bin/dockerd-entrypoint.sh > /dev/null 2>&1 &
 
-exec "$@"
+# Wait until started docker
+if timeout 30 bash -c 'until docker info > /dev/null 2>&1; do sleep 1; done'
+then
+    exec "$@"
+else
+    echo '[ERROR] Failed to launch docker. Is the --privileged option enabled?' >&2
+fi
